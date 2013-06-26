8abc1a43dce63e3204fc32af6af60ada0eb3afd1
Zach Freeman <zfreeman.nu@gmail.com>
2013-06-26 13:17:05 -0500

    Added a template

--- ttt/views.py
+++ ttt/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def base(request):
     return HttpResponse("I, Zach Freeman, have just created a Heroku-based website!")
 
 def ttt_base(request):
-    return HttpResponse("You're at the base page for the ttt app!")
+    return render(request,'ttt/base.html',{})
