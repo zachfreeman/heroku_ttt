eb8531946028602de3ec72fc1e1ec447a65ca9ea
Zach Freeman <zfreeman.nu@gmail.com>
2013-06-27 13:37:41 -0500

    Updated file

--- ttt/views.py
+++ ttt/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def ttt_game(request):
-    return HttpResponse(request.META['HTTP_HOST'])
-    # return render(request,'ttt/game.html',{})
+    # return HttpResponse(request.META['HTTP_HOST'])
+    return render(request,'ttt/game.html',{})
