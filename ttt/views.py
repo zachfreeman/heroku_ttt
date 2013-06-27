fc4005405a8cae1f198e5d575510c8502dfceeaa
Zach Freeman <zfreeman.nu@gmail.com>
2013-06-27 13:32:37 -0500

    determining host header

--- ttt/views.py
+++ ttt/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def ttt_game(request):
-    return render(request,'ttt/game.html',{})
+    return HttpResponse(request.META['HTTP_HOST'])
+    # return render(request,'ttt/game.html',{})
