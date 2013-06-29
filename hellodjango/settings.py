e4c487fc98323928564357ee0235a907bd3bc114
Zach Freeman <zfreeman.nu@gmail.com>
2013-06-29 12:39:45 -0500

    Trying to connect to DB without environmental variables - dj_database_url module may handle it

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -11,13 +11,20 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.environ.get('DBASE_NAME'),                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',               # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': os.environ.get('DBASE_USER'),
-        'PASSWORD': os.environ.get('DBASE_PW'),
-        'HOST': 'ec2-54-225-121-179.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '5432',                      # Set to empty string for default.
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        # 'NAME': os.environ.get('DBASE_NAME'),               # Or path to database file if using sqlite3.
+        # # The following settings are not used with sqlite3:
+        # 'USER': os.environ.get('DBASE_USER'),
+        # 'PASSWORD': os.environ.get('DBASE_PW'),
+        # 'HOST': 'ec2-54-225-121-179.compute-1.amazonaws.com', # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        # 'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
