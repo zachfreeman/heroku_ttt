ec76cf1c86dedb7a164d2f788b03d21896c403d9
Zach Freeman <zfreeman.nu@gmail.com>
2013-06-29 13:16:31 -0500

    Added test model, but messed up South implementation, won't try to use here

--- ttt/models.py
+++ ttt/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=200,blank=True,default="")
+    last_name = models.CharField(max_length=200,blank=True,default="")
+    school = models.CharField(max_length=200,blank=True,default="")
+    industry = models.CharField(max_length=200,blank=True,default="")
+
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name
