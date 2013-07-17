1e6fd8bc411cb186a0126d393ece49d6b0b618c2
Zach Freeman <zfreeman.nu@gmail.com>
2013-07-17 13:28:11 -0400

    Updated 3D version - it flows smoothly

--- ttt/urls.py
+++ ttt/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('ttt.views',
-    url(r'^2d$','ttt_2d',name='ttt_2d'),
+    url(r'^2d/$','ttt_2d',name='ttt_2d'),
+    url(r'^3d/$','ttt_3d',name='ttt_3d'),
+    url(r'^3d_test/$','ttt_3d_test',name='ttt_3d_test'),
+    url(r'^3d_2/$','ttt_3d_2',name='ttt_3d_2'),
 )
--- ttt/views.py
+++ ttt/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def ttt_2d(request):
     return render(request,'ttt/game.html',{})
+
+def ttt_3d(request):
+    return render(request,'ttt/three_d.html',{})
+
+def ttt_3d_test(request):
+    return render(request,'ttt/three_d_test.html',{})
+
+def ttt_3d_2(request):
+    return render(request,'ttt/three_d_2.html',{})
